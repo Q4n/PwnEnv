9f33e9e0b25e51fa0500bbe6550b37fa18c11343
Q4n <1677609544@qq.com>
2019-11-27 00:04:29 +0800

    add some package

--- Scripts/misc.sh
+++ Scripts/misc.sh
@@ -1,5 +1,5 @@
 # sudo apt-get install open-vm-tools-desktop fuse
-sudo apt-get install vim net-tools libc-dbg python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential ruby curl zsh lib32z1 gcc-multilib --fix-missing -y
+sudo apt-get install vim net-tools python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential ruby curl zsh lib32z1 gcc-multilib --fix-missing -y
 
 mkdir ~/.pip
 echo "[global]" >> ~/.pip/pip.conf
@@ -10,3 +10,5 @@ sudo gem install one_gadget
 
 sudo apt-get install ipython ipython3 -y --fix-missing
 
+sudo dpkg --add-architecture i386
+sudo apt install -y libc6:i386 libc6-dbg:i386 libc6-dbg lib32stdc++6 cmake iputils-ping python3-dev ruby-dev tmux strace ltrace nasm radare2 gdb gdb-multiarch  netcat socat patchelf file python3-distutils bison --fix-missing
